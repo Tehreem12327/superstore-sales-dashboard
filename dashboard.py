a107d88bed7c707d4aac88e4169fc499641940b3
Tehreem Rasheed <bsf23006469@ue.edu.pk>
2026-09-18 16:06:59 +0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -15,7 +15,7 @@ if fl is not None:
     st.write(filename)
     df = pd.read_csv(fl, encoding="ISO-8859-1")
 else:
-    df = pd.read_csv(r"C:\Users\hp\Desktop\Superstore.csv", encoding="ISO-8859-1")
+        df = pd.read_csv("Superstore.csv", encoding="ISO-8859-1")
 
 
 col1, col2 = st.columns((2))
